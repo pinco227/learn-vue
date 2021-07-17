2ac94bca0eccd83363e27d4667924075260fa3da
Paul Istratoaie <p4ul.gg@gmail.com>
2021-07-17 21:13:31 +0100

    Writing my first vue instance

---
+++ hello_vue.js
@@ -0,0 +1,9 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        message: "Hello World!",
+        value: 5,
+        imgSrc: "https://cdn.pixabay.com/photo/2016/08/24/14/29/earth-1617121_960_720.jpg",
+        link: "https://vuejs.org"
+    }
+})
